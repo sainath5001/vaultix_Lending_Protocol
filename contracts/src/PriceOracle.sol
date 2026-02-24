7f07fd6e7c3f879b3990010e4de953b7bf38b57d
Sainath More <sainathmore501@gmail.com>
2026-02-24 04:05:11 +0000

    Add PriceOracle and VaultToken, integrate Chainlink price feeds

---
+++ contracts/src/PriceOracle.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract PriceOracle is Ownable {
+
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
+    mapping(address => address) public priceFeeds;
+
+    function setPriceFeed(address asset, address feed)
+        external
+        onlyOwner
+    {
+        require(asset != address(0), "Invalid asset");
+        require(feed != address(0), "Invalid feed");
+
+        priceFeeds[asset] = feed;
+    }
+
+    function getPrice(address asset)
+        external
+        view
+        returns (uint256)
+    {
+        address feed = priceFeeds[asset];
+        require(feed != address(0), "Feed not set");
+
+        AggregatorV3Interface priceFeed =
+            AggregatorV3Interface(feed);
+
+        (
+            ,
+            int256 answer,
+            ,
+            ,
+            
+        ) = priceFeed.latestRoundData();
+
+        require(answer > 0, "Invalid price");
+
+        uint8 decimals = priceFeed.decimals();
+
+        return uint256(answer) * (10 ** (18 - decimals));
+    }
+}
---
+++ contracts/src/VaultToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract VaultToken is ERC20 {
+
+    address public lendingPool;
+
+    modifier onlyLendingPool() {
+        require(msg.sender == lendingPool, "Not LendingPool");
+        _;
+    }
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address lendingPool_
+    ) ERC20(name_, symbol_) {
+        require(lendingPool_ != address(0), "Invalid pool");
+        lendingPool = lendingPool_;
+    }
+
+    function mint(address to, uint256 amount)
+        external
+        onlyLendingPool
+    {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount)
+        external
+        onlyLendingPool
+    {
+        _burn(from, amount);
+    }
+}
